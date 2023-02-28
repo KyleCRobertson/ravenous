5be2a53c3f67713e3c2c18640f378981fea3eef9
Kyle Robertson <91642555+KyleCRobertson@users.noreply.github.com>
2023-02-27 19:19:40 -0500

    Initial Ravenous Commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,61 @@
+const apiKey = 'bUx3HUe14tz8QpBGIa_Flk_YIHuoXH5vX_3Ygg1j70geIMtzu__JhAKdJ4uRs2pQDGTinQHk9D62DLoidNblYgBefL8agxpL7f0FTaJwkCgjQgbcWqZQJr4uE9HWY3Yx';
+
+const Yelp = { 
+    search(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(jsonResponse.businesses){
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                });
+            }
+        });
+    }
+};
+
+/*
+const Yelp = {
+    search(term, location, sortBy) {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      }).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }));
+        }
+      });
+    }
+  };
+*/
+
+export default Yelp;
